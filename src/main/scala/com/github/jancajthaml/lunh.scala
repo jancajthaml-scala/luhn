a0cec3238ddf92906b8cde0fcacb2e2222acdca7
Jan Cajthaml <jan.cajthaml@gmail.com>
2016-10-14 21:08:42 +0200

    sped up

--- src/main/scala/com/github/jancajthaml/lunh.scala
+++ src/main/scala/com/github/jancajthaml/lunh.scala
@@ -13,11 +13,9 @@ object luhn_checksum extends ((String) => Int) {
    * @return check_digit
    */
   def apply(number: String): Int = {
-    val sanitized = (number + '0').replaceFirst("^0+(?!$)", "").reverse.map(_.toInt)
-    val odd = Iterator.from(1, 2).takeWhile(_ < sanitized.size).map(sanitized(_))
-    val even = sanitized.filterNot(odd.toSet).map(_.toInt << 1)
-    val check = (odd.sum + even.sum) & 0x1
-    if (check == 0) 0 else (10 - check)
+    val set = (number + '0').reverse
+    val digit = set.indices.map(i => set(i).toInt << i & 0x1).sum & 0x1
+    if (digit == 0) 0 else (10 - digit)
   }
 
 }
